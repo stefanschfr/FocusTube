15f926070f1502b923d230ea104861f964a017ff
stefanschfr <sfschaefer@web.de>
2026-08-02 22:39:16 +0200

    feat: remove thumbnails and other distractions from search

--- rules.js
+++ rules.js
@@ -5,7 +5,8 @@ const RULES = {
 
         'yt-tab-shape[tab-title="Shorts"]', // Channel Short Category
         'ytd-reel-shelf-renderer', // Shorts from Channel
-        '.tabGroupShapeSlider' // Channel Category Slider
+        '.tabGroupShapeSlider', // Channel Category Slider
+        'yt-thumbnail-view-model' // Thumbnails on Profiles and Feed
     ],
 
     "/": [
@@ -18,11 +19,23 @@ const RULES = {
 
     "/watch": [
         '#secondary', // Sidebar Suggestions
-        '#panels-full-bleed-container' // Sidebar Panel in Fullscreen
+        '#panels-full-bleed-container', // Sidebar Panel in Fullscreen
+        '.ytp-ce-element', // Profile and other Video Link
+        '.ytp-suggestion-set' // Suggestions after Video
     ],
     "/results": [
         'grid-shelf-view-model', // Shorts from Search
         '#chip-bar', // Search Filter Bar
-        'a[href="https://www.youtube.com/howyoutubeworks/product-features/search/"]' // Search Infos
+        'a[href="https://www.youtube.com/howyoutubeworks/product-features/search/"]', // Search Infos
+        'ytd-thumbnail', // Thumbnails in Search
+        'ytd-horizontal-card-list-renderer', //Similar Searches
+        'ytd-badge-supported-renderer', // New and 4K Badge
+        '.metadata-snippet-container-one-line', //Video description
+        '.metadata-snippet-container ', //Video description
+        '#description-text', //Video description
+        '#expandable-metadata', //Playlists
+        'ytd-universal-watch-card-renderer', //Videos on Sidebar
+        'ytd-shelf-renderer', //Suggestions
+        'ytd-channel-renderer', //Channels
     ]
 };
